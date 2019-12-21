2172243e19242475154ffe7b3be67d132c8ce5f4
BCopos <bcopos@ucdavis.edu>
2019-12-21 12:09:21 -0800

    update process articles code

--- src/process_articles.py
+++ src/process_articles.py
@@ -12,25 +12,29 @@ name, url, content, description, scraper = foo()
 article = file_utils.createArticle(name, url, content, description, scraper)
 # dict: article['name'] or article.get('name')
 # namedtuple: article.name
-articles.appedn(article)
+article.scraper = 'newsapi'
+articles.append(article)
+
 
 # 2. get content per article
-
-
+#urls = ['https://www.cnn.com/2019/12/10/media/netflix-stock-sell/index.html']
+#for url in urls:
 news_scraper = content_scraper.NewsUrlContentScraper()
-urls = ['https://www.cnn.com/2019/12/10/media/netflix-stock-sell/index.html']
-for url in urls:
-    content = news_scraper.getArticleContent(url)
+for article in articles:
+    content = news_scraper.getArticleContent(article.url)
     print(content)
+    # copy article namedtuple
+    article.scraper = 'content_scraper'
+    article.content = content
     print('=============\n\n')
-    keywords = news_scraper.getArticleKeywords(url)
+    keywords = news_scraper.getArticleKeywords(article.url)
     print(keywords)
     print('=============\n\n')
-    summary = news_scraper.getArticleSummary(url)
+    summary = news_scraper.getArticleSummary(article.url)
     print(summary)
 
 
 # 3. write to file
 for article in articles:
-    file_utils.SaveArticleDetailsToFile(article)
+file_utils.SaveArticleDetailsToFile(article)
 
