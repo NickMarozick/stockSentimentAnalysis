231a947e47f6acab72e91d3e55682fbb2e098602
BCopos <bcopos@ucdavis.edu>
2021-01-30 12:40:45 -0800

    cleaned up test code and small improvements to user loop

--- src/stock_historical_data/getStockData.py
+++ src/stock_historical_data/getStockData.py
@@ -8,27 +8,41 @@ def setTicker(ticker):
     return yahoo_financials
 
 def getStockDataAllTime(ticker):
-    today= helper_functions.getTodaysDate()
-    yahoo_financials= setTicker(ticker)
-    historicalStockPrices= yahoo_financials.get_historical_price_data("1816-01-01", today, "daily")
-    prices= historicalStockPrices[ticker]['prices']
+    today = helper_functions.getTodaysDate()
+    yahoo_financials = setTicker(ticker)
+    historicalStockPrices = yahoo_financials.get_historical_price_data(
+        "1816-01-01", today, "daily")
+    prices = historicalStockPrices[ticker]["prices"]
     return prices
 
 def getAndStoreStockPricingData(ticker, conn):
     prices = getStockDataAllTime(ticker)
     sqlite_utils.insertPrices(conn, ticker, prices)
 
+def _getPriceDataForTicker(ticker, conn):
+    try:
+        prices = getStockDataAllTime(ticker)
+        getAndStoreStockPricingData(ticker, conn)
+    except Exception as e:
+        print("Invalid StockSymbol!")
+
+
 def userSearchAndStoreStockPricingLoop(conn):
+    """Search and store prices for stock based on user input.
+
+    Args:
+        conn: <class name of conn... same as type(conn)>
+    """
     while True:
-        ticker = input('Enter a stock symbol or to quit program, type done: ')
-        if ticker == "done" :
+        tickers = input("Enter stock symbol(s) (e.g., APPL,AMC) or 'quit' to "
+                        "end: ")
+        if tickers == "quit" :
             break
-            #sys.exit(0)
-
         else:
-            try:
-                prices= getStockDataAllTime(ticker)
-                getAndStoreStockPricingData(ticker, conn)
+            if "," in tickers:
+                ticker = ticker.strip()
+                for ticker in tickers.split(","):
+                    _getPriceDataForTicker(ticker, conn)
+            else:
+                _getPriceDataForTicker(ticker, conn)
 
-            except Exception as e:
-                print("Invalid StockSymbol, no data", e)
