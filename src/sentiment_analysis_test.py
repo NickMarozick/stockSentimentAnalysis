5d995bacee7cd15d3dd4e2d99eee102631af21c5
BCopos <bcopos@ucdavis.edu>
2021-02-25 21:34:29 -0800

    minor updates to sklearn_sa.py

--- src/sentiment_analysis_test.py
+++ src/sentiment_analysis_test.py
@@ -1,5 +1,28 @@
 from sentiment_analysis_utils import sklearn_sa
 
- = sklearn_sa.TransformData()
- = sklearn_sa.Train()
- = sklearn_sa.Fit()
+#import xml.etree.ElementTree as ET
+#t = ET.parse('/Users/bcopos/Downloads/sorted_data/software/negative.review')
+#print(t)
+#print(root)
+
+
+from lxml import etree
+from lxml import html
+
+tree = html.parse('/Users/bcopos/Downloads/sorted_data/kitchen_&_housewares/negative.review')
+#print(dir(tree))
+#print(tree.xmlschema())
+
+from bs4 import BeautifulSoup
+f = open('/Users/bcopos/Downloads/sorted_data/kitchen_&_housewares/negative.review', 'rb')
+data = f.read()
+f.close()
+soup = BeautifulSoup(data)
+reviews = soup.find_all('review_text')
+for review in reviews:
+    print(review.text.strip())
+
+
+#a = sklearn_sa.TransformData()
+#b = sklearn_sa.Train()
+#c = sklearn_sa.Fit()
--- src/sentiment_analysis_utils/sklearn_sa.py
+++ src/sentiment_analysis_utils/sklearn_sa.py
@@ -1,10 +1,22 @@
 """Functions for performing sentiment analysis using sklearn.
 
 
+What's done:
+- reads data from files (unzipped directories from the website)
+- creates training data and training labels
+
+To do:
+- check that TfidfTransformer works (this translates raw training data text
+  into feature vectors)
+- finish Fit() function (make sure the parameter is correct)
+- test
+
 """
+
 
 import os
 
+from bs4 import BeautifulSoup
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
@@ -16,6 +28,7 @@ CLASSIFIER_TYPE = MultinomialNB
 #CLASSIFIER = Train()
 #CLASSIFIER_CLASSES = CLASSIFIER.classes_
 
+
 def TransformData(text):
     """Converts a list of articles into features vectors."""
     # Option #1: word count
@@ -26,16 +39,6 @@ def TransformData(text):
     features_nd = features.toarray() # for easy usage
     return text_tfidf
 
-def Train():
-    """Trains classifier."""
-    training_data = []
-    for training_corpus in TRAINING_CORPORA:
-        f = open(training_corpus, 'r')
-        content = f.read()
-        training_data.append(content.split())
-
-    training_tfidf = TransformData(training_data)
-    return CLASSIFIER_TYPE.fit(training_data, TRAINING_LABELS)
 
 def Fit(article):
     """Takes an article object and classifies it."""
@@ -49,27 +52,45 @@ def Fit(article):
     print('Results: ', zip(CLASSIFIER_CLASSES, predicted_probs))
 
 
-def ParseFile(data_file):
-    f = open(data_file, 'r')
-    lines = f.readlines()
-    for line in lines:
-        data = line.split()
-        label = data[-1].split(':')[-1]
-        for token in data[:-1:]:
-            word, count = token.split(':')
-            yield (word, count, label)
-        break
+def ReadTrainingDataFile(data_file):
+    f = open(data_file, 'rb')
+    data = f.read()
+    f.close()
+    soup = BeautifulSoup(data)
+    reviews = []
+    try:
+        reviews = soup.find_all('review_text')
+        reviews = [review.text.strip() for review in reviews]
+    except Exception as e:
+        print('Error (%s) while processing training data file %s',
+              str(e), data_file)
+    return reviews
 
 
 def ReadTrainingData():
     """Read data from http://www.cs.jhu.edu/~mdredze/datasets/sentiment/"""
-    data_dir = '/Users/bcopos/Downloads/processed_acl/books'
-    feature_vector = dict()
-    for (dirpath, dirnames, filenames) in os.walk(data_dir): 
-        print(dirpath, dirnames, filenames)
-        for f in filenames:
-            for word, count, label in ParseFile(os.path.join(dirpath, f)):
-                print(word, count, label)
-                pass
+    data_directory = '/Users/bcopos/Downloads/sorted_data'
+    negative_data = []
+    positive_data = []
+    for root, dirs, files in os.walk(data_directory):
+        if 'negative.review' in files:
+            negative_data.extend(ReadTrainingDataFile(
+                os.path.join(root, 'negative.review')))
+        if 'positive.review' in files:
+            positive_data.extend(ReadTrainingDataFile(
+                os.path.join(root, 'positive.review')))
+    return (positive_data, negative_data)
 
-ReadTrainingData()
+
+def Train():
+    """Trains classifier."""
+    positive_data, negative_data = ReadTrainingData()
+    training_data = positive_data
+    training_data.extend(negative_data)
+    training_labels = [1 for i in positive_data]
+    training_labels.extend([-1 for i in negative_data])
+    training_tfidf = TransformData(training_data)
+    return CLASSIFIER_TYPE.fit(training_data, TRAINING_LABELS)
+
+
+Train()
