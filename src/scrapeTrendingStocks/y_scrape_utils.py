598cb6e3ed90b51c49c4a0066772b68527c849fe
NickMarozick <nmarozick@gmail.com>
2021-03-14 12:47:00 -0700

    added scraping capability for most active stocks, top gainers, top losers

---
+++ src/scrapeTrendingStocks/y_scrape_utils.py
@@ -0,0 +1,36 @@
+import re
+
+
+def get_ticker(tableRowString):
+    ticker = re.search('href="\/quote\/([A-Z-]*)\?p=([A-Z-]*)"', tableRowString)
+    ticker = ticker.group()
+    ticker = (re.search('\/[A-Z-]*\?', ticker)).group()
+    ticker = ticker[1:len(ticker)-1]
+
+    return ticker
+
+def get_trade_volume(tableRowString):
+    #tradeVolume = re.search('[0-9.]*M<\/span><\/td><td aria-label="Avg Vol \(3 month\)', tableRowString)
+    tradeVolume = re.search('[0-9.,M]*<\/span><\/td><td aria-label="Avg Vol \(3 month\)', tableRowString)
+    tradeVolume = tradeVolume.group()
+    #tradeVolume = (re.search('[0-9]*.[0-9]*M',tradeVolume)).group()
+    tradeVolume = (re.search('[0-9,.M]*',tradeVolume)).group()
+
+    return tradeVolume
+
+def get_change_percentage(tableRowString):
+    changePercentage = re.search('data-reactid="[0-9]*">([+-][0-9]*|0)*.[0-9]*%', tableRowString)
+    changePercentage = changePercentage.group()
+    changePercentage = (re.search('([+-][0-9]*|0)*.[0-9]*%', changePercentage)).group()
+
+    return changePercentage
+
+def get_avg_3_month_volume(tableRowString):
+    #avg3MonthVolume= re.search('Avg Vol \(3 month\)" class="Va\(m\) Ta\(end\) Pstart\(20px\) Fz\(s\)" colspan="" data-reactid="[0-9]*"><!-- react-text: [0-9]* -->[0-9]*[.,][0-9M]*', tableRowString)
+    avg3MonthVolume= re.search('Avg Vol \(3 month\)" class="Va\(m\) Ta\(end\) Pstart\(20px\) Fz\(s\)" colspan="" data-reactid="[0-9]*"><!-- react-text: [0-9]* -->[0-9.,M]*', tableRowString)
+    avg3MonthVolume = avg3MonthVolume.group()
+    #avg3MonthVolume= (re.search('>[0-9]*[.,][0-9M]*', avg3MonthVolume)).group()
+    avg3MonthVolume= (re.search('>[0-9.,M]*', avg3MonthVolume)).group()
+    avg3MonthVolume= avg3MonthVolume[1:]
+
+    return avg3MonthVolume
---
+++ src/scrapeTrendingStocks/yahooScrape.py
@@ -0,0 +1,60 @@
+import requests
+from bs4 import BeautifulSoup
+import y_scrape_utils
+
+
+# setting up our URLs to scrape
+mostActiveURL = "https://finance.yahoo.com/most-active?offset=0&count=100"
+lossURL = "https://finance.yahoo.com/losers?offset=0&count=100"
+gainURL = 'https://finance.yahoo.com/gainers?offset=0&count=100'
+
+# getting content
+mostActiveRequest = requests.get(mostActiveURL)
+lossRequest = requests.get(lossURL)
+gainRequest = requests.get(gainURL)
+
+# parsing content via beautifulSoup
+soupMostActive = BeautifulSoup(mostActiveRequest.content, 'html.parser')
+soupLoss = BeautifulSoup(lossRequest.content, 'html.parser')
+soupGain = BeautifulSoup(gainRequest.content, 'html.parser')
+
+# initiating our dictionaries for the results
+mostActiveStocks = {}
+losingStocks = {}
+gainingStocks = {}
+
+# gathering chart content
+mostActiveTable = soupMostActive.findAll('tr', attrs = {'class':'simpTblRow'})
+lossTable = soupLoss.findAll('tr', attrs = {'class':'simpTblRow'})
+gainTable = soupGain.findAll('tr', attrs = {'class':'simpTblRow'})
+
+# iterating and utilizing the regex functions to pull data
+for i in mostActiveTable:
+    ticker = y_scrape_utils.get_ticker(str(i))
+    tradeVolume = y_scrape_utils.get_trade_volume(str(i))
+    changePercentage = y_scrape_utils.get_change_percentage(str(i))
+    avg3MonthVolume = y_scrape_utils.get_avg_3_month_volume(str(i))
+    mostActiveStocks[ticker] = changePercentage, tradeVolume, avg3MonthVolume
+
+
+for j in lossTable:
+    ticker = y_scrape_utils.get_ticker(str(j))
+    tradeVolume = y_scrape_utils.get_trade_volume(str(j))
+    changePercentage = y_scrape_utils.get_change_percentage(str(j))
+    avg3MonthVolume = y_scrape_utils.get_avg_3_month_volume(str(j))
+    losingStocks[ticker] = changePercentage, tradeVolume, avg3MonthVolume
+
+for k in gainTable:
+    ticker = y_scrape_utils.get_ticker(str(k))
+    tradeVolume = y_scrape_utils.get_trade_volume(str(k))
+    changePercentage = y_scrape_utils.get_change_percentage(str(k))
+    avg3MonthVolume = y_scrape_utils.get_avg_3_month_volume(str(k))
+    gainingStocks[ticker] = changePercentage, tradeVolume, avg3MonthVolume
+
+
+print("Most Active Stocks: ", mostActiveStocks)
+print("\n")
+print("Losing Stocks: ", losingStocks)
+print("\n")
+print("Gaining Stocks: ", gainingStocks)
+print("\n")
