36f751efe6012b1cef51f6618d560fb6d869ab23
BCopos <bcopos@ucdavis.edu>
2021-04-27 19:44:56 -0700

    Fix Fit() function for sentiment analysis

--- src/sentiment_analysis_utils/sklearn_sa.py
+++ src/sentiment_analysis_utils/sklearn_sa.py
@@ -21,7 +21,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 
-# TODO: there are better design patterns than using globals. Globals are 
+# TODO: there are better design patterns than using globals. Using globals is
+# discouraged.
 COUNT_VECTORIZER = None
 TFIDF = None
 CLASSIFIER = None
@@ -46,14 +47,13 @@ def TransformData(text):
 
 def Fit(text):
     """Takes an article object and classifies it."""
-    # TODO: check that article.text is correct
     article_tfidf = TransformData([text])
-    predicted_probs = CLASSIFIER_TYPE().predict_proba(article_tfidf)
+    global CLASSIFIER
+    predicted_probs = CLASSIFIER.predict_proba(article_tfidf)
     # the output shoud be an array with two elements, one corresponding to
     # probability it's a positive sentiment and the other corresponding to
-    # probability it's a negative sentiment. The order matches the order of
-    # CLASSIFIER_CLASSES.
-    print('Results: ', zip(CLASSIFIER_CLASSES, predicted_probs))
+    # probability it's a negative sentiment.
+    return list(zip(CLASSIFIER.classes_, predicted_probs[0]))
 
 
 def ReadTrainingDataFile(data_file):
@@ -89,21 +89,17 @@ def ReadTrainingData():
 def Train():
     """Trains classifier."""
     positive_data, negative_data = ReadTrainingData()
-    print('Positive data len %s', len(positive_data))
-    print('Negative data len %s', len(negative_data))
     training_labels = [1 for i in positive_data]
     training_labels.extend([-1 for i in negative_data])
-    print('Labels len %s', len(training_labels))
     training_data = []
     training_data.extend(positive_data)
     training_data.extend(negative_data)
-    print('Training data len %s', len(training_data))
     training_tfidf = TransformData(training_data)
     global CLASSIFIER
     CLASSIFIER = CLASSIFIER_TYPE()
     CLASSIFIER.fit(training_tfidf, training_labels)
 
 
-Train()
-Fit('some article with good news')
+#Train()
+#Fit('some article with good news')
 
