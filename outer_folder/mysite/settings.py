b6ae624f26031ab21bfd0f75711bf4a723c95ac6
NickMarozick <nmarozick@gmail.com>
2022-01-24 22:05:48 -0800

    added redis and celery requirements. created a sample tasks.py

--- outer_folder/mysite/settings.py
+++ outer_folder/mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_beat',
+    'django_celery_results',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_RESULT_BACKEND = "django-db"
