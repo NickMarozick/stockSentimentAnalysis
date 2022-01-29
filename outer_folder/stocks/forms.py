3c8f50dc57e505044a7fc75457c8864febd4f33b
NickMarozick <nmarozick@gmail.com>
2022-01-29 11:41:34 -0800

    edited stock form

--- outer_folder/stocks/forms.py
+++ outer_folder/stocks/forms.py
@@ -6,6 +6,6 @@ from .models import StockSymbol
 class SelectStockForm(forms.ModelForm):
     class Meta:
         model = StockSymbol
-        fields = ['user_selected']
+        fields = ['id']
     
     select_stock = ModelMultipleChoiceField(queryset=StockSymbol.objects.all(), widget=forms.CheckboxSelectMultiple)
