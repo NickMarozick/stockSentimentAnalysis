aa807901d5d3b7a8a7f7ac70290989c51e373739
NickMarozick <nmarozick@gmail.com>
2022-02-05 19:33:08 -0800

    changed to psql

--- outer_folder/mysite/settings.py
+++ outer_folder/mysite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'stock_project_database',
+        'USER': 'stock_project_database_user',
+        'PASSWORD': 'test',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
