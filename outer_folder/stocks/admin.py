e46010434bf34262f58a49778ce2a64f1f905917
NickMarozick <nmarozick@gmail.com>
2022-02-17 21:40:05 -0800

    fixed admin display to show all fields for each stock model

--- outer_folder/stocks/admin.py
+++ outer_folder/stocks/admin.py
@@ -2,4 +2,29 @@ from django.contrib import admin
 
 from .models import StockSymbol, StockArticle, StockLoser, StockGainer, PriceData
 # Register your models here.
-admin.site.register([StockGainer, StockArticle, StockLoser, PriceData, StockSymbol])
+
+class StockSymbolAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'user_selected')
+
+class StockArticleAdmin(admin.ModelAdmin):
+    list_display = ('stock', 'date', 'name', 'sentiment', 'url', 'content', 'description')
+
+class PriceDataAdmin(admin.ModelAdmin):
+    list_display = ('stock', 'date', 'open', 'close', 'volume')
+
+class StockGainerAdmin(admin.ModelAdmin):
+    list_display = ('stock', 'date', 'change_percentage', 'price', 'trade_volume', 'avg_3_month_volume')
+
+class StockLoserAdmin(admin.ModelAdmin):
+    list_display = ('stock', 'date', 'change_percentage', 'price', 'trade_volume', 'avg_3_month_volume')
+
+
+admin.site.register(StockSymbol, StockSymbolAdmin)
+
+admin.site.register(StockArticle, StockArticleAdmin)
+
+admin.site.register(PriceData, PriceDataAdmin)
+
+admin.site.register(StockGainer, StockGainerAdmin)
+
+admin.site.register(StockLoser, StockLoserAdmin)
